7d2767b15cf929f3242965f4bbb3794c0613390f
Abdullah Awad <abdullah.alashafi@gmail.com>
2021-10-23 22:15:55 +0300

    create first app for store memories

---
+++ memoryhome/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from datetime import datetime
+
+class Memory(models.Model):
+    memory = models.TextField(_("memory body"))
+    created = models.DateTimeField(_("created"),auto_now_add=True)
+    photo = models.ImageField(_("memory image"), upload_to='images', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.memory[:10]} - {datetime.strftime(self.created, '%d-%m-%y %H:%M:%S')}"
